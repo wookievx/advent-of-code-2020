402da17ae83649c0deb331ca9843e706b72a1ba4
ŁukaszLampart <ll@ll.ll>
2020-12-02 18:24:58 +0100

    Day 2, fighting with memory model

---
+++ rust/src/day2.rs
@@ -0,0 +1,64 @@
+extern crate nom;
+
+use nom::{
+    IResult,
+    bytes::complete::tag,
+    combinator::map_res,
+    sequence::tuple};
+use nom::sequence::separated_pair;
+use nom::bytes::complete::take_while;
+use nom::character::{is_digit, is_alphabetic, is_space};
+use self::nom::sequence::preceded;
+use self::nom::number::complete::u16;
+use self::nom::bytes::complete::take;
+use self::nom::AsChar;
+
+pub struct Password {
+    min_req: u16,
+    max_req: u16,
+    character: char,
+    content: String
+}
+
+pub fn solve(input: &Vec<Password>) -> usize {
+    input
+        .iter()
+        .filter(|p| {
+            let count = p.content.as_str().matches(|c: char| c == p.character).count();
+            let res = usize::from(p.min_req) <= count && count <= usize::from(p.max_req);
+            res
+        })
+        .count()
+}
+
+pub fn solve_adv(input: &Vec<Password>) -> usize {
+    input
+        .iter()
+        .filter(|p| {
+            let chars: Vec<char> = p.content.chars().collect();
+            let res = (chars[usize::from(p.min_req) - 1] == p.character) ^ (chars[usize::from(p.max_req) - 1] == p.character);
+            res
+        })
+        .count()
+}
+
+pub fn parse_input(input: Vec<String>) -> Vec<Password> {
+    input
+        .iter()
+        .map(|contents| parse_line(contents.as_str()).unwrap().1)
+        .collect()
+}
+
+fn parse_line(input: &str) -> IResult<&str, Password> {
+    let (input, (min_req, max_req)) = separated_pair(parse_number, tag("-"), parse_number)(input)?;
+    let (input, (character, content)) = preceded(take_while(|c: char| c.is_whitespace()), separated_pair(take(1usize), tag(": "), take_rest))(input)?;
+    IResult::Ok(("", Password { min_req, max_req, character: character.chars().next().unwrap(), content: content.to_string() }))
+}
+
+fn parse_number(input: &str) -> IResult<&str, u16> {
+    map_res(take_while(|c: char| c.is_dec_digit()), |s: &str| s.parse::<u16>())(input)
+}
+
+fn take_rest(input: &str) -> IResult<&str, &str> {
+    IResult::Ok(("", input))
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 mod utils;
 mod day1;
+mod day2;
 
 fn main() {
     let args1: Vec<u32> =
@@ -18,5 +19,12 @@ fn main() {
     } else {
         println!("Failed to find solution for day1");
     }
+
+    let args2: Vec<String> = utils::files::lines_from_file("resources/day2/input.txt");
+    let parsed_day2 = day2::parse_input(args2);
+    let res2 = day2::solve(&parsed_day2);
+    let res_as2adv = day2::solve_adv(&parsed_day2);
+    println!("Result of day2: {res}", res = res2);
+    println!("Result of day2(part 2): {res}", res = res_as2adv);
     println!("Hello, world!");
 }
